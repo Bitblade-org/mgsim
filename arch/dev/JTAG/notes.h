05968bdb10b9f81e3901a36c98001493b6b253d5
Mark Laagland <github@bestpc.or>
2015-07-30 10:26:31 +0200

    Added notes.h in JTAG

---
+++ arch/dev/JTAG/notes.h
@@ -0,0 +1,22 @@
+//			MLDQUESTION Hoe weet ik (vanuit main) op welke core een thread terecht is gekomen? (Liefst devId van core) --> Eerst create---sync
+
+    //MLDNOTE IOInterface heeft details over ASR info IO.
+
+//			MLDQUESTION Hoe weet ik het lokale IO DeviceId? --> ASR
+
+//			MLDQUESTION Klopt het dat het geheugen van detatched threads nooit "synct"? --> Half
+
+//			MLDQUESTION sep_alloc voor 1 core faalt in 4-core arch --> Klopt
+
+//			MLDQUESTION storagetraces enorm groot bij 8 cores met IO --> Auw
+
+//			MLDQUESTION Fixed IO adressen voor HW? Waarom al de berichten? --> Unknwon
+
+//			MLDQUESTION Kan geen fixed IO adres opgeven (holes) --> Whoopsie
+
+
+//		SL Document, sect 4.3, resources ^ placement'
+
+//		Boek appendix E
+
+//		Boek 11.2, (Boek 11.3)
